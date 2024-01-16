00d1c631c43b37efffe348d1ff128460989889d7
omniaatefmohamed <omnia.atef.mohamed@gmail.com>
2024-01-16 13:29:16 +0200

    ww

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ const globalError = require('./middlewares/errorMiddleware');
 const mountRoutes = require('./routes');
 const { webhookCheckout } = require('./controllers/orderService');
 
-const dbConnection = require('./config/database');
-
+// const dbConnection = require('./config/database');
+mongoose.connect('mongodb+srv://omnia:omnia@cluster1.tqahmie.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB', err));
 // const categoryRouter = require('./routes/categoryRoute');
 // const subCategoryRouter = require('./routes/subCategoryRoute');
 // const brandRouter = require('./routes/brandRoute');
@@ -30,7 +35,7 @@ const dbConnection = require('./config/database');
 // const couponRouter = require('./routes/couponRoute');
 
 // DB Connection
-dbConnection();
+// dbConnection();
 
 // Builtin Middleware
 const app = express();
